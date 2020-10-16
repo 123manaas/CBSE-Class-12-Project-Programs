f19f95814b5f7992e92d0b4108f81269cf2e270f
Deepak-the-coder <72347873+Deepak-the-coder@users.noreply.github.com>
2020-10-16 08:46:33 +0530

    Delete Project 6.py

--- Project 6.py
+++
@@ -1,7 +0,0 @@
-'''
-Project question -  Write a random number generator that generates random numbers
-                    between 1 and 6 (simulates a dice). 
-'''
-# Solution:-
-import random
-print('The random number is=',random.randrange(1,7))
